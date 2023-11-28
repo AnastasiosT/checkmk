3266b9b58d5352f4e6bf965ef2c90bd065b7b8b8
Anastasios Thomaidis <anastasios.thomaidis@checkmk.com>
2023-11-28 15:31:20 -0500

    a

---
+++ REST-API/distributed_connections.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import pprint, json
+
+
+site = {
+        "alias": 
+        ["alias_site_id_1", 
+        "alias_site_id_2",
+        "alias_site_id_3",
+        "alias_site_id_4",
+        "alias_site_id_5",
+        "alias_site_id_6",
+        "alias_site_id_7",
+        "alias_site_id_8",
+        "alias_site_id_9",
+        "alias_site_id_10",
+        "hay"
+
+        ],
+
+        "site": ["site_id_1",
+                "site_id_2",
+                "site_id_3",
+                "site_id_4",
+                "site_id_5",
+                "site_id_6",
+                "site_id_7",
+                "site_id_8",
+                "site_id_9",
+                "site_id_10",
+                "hay"
+                ],
+                
+        "url_prefix": [
+                "http://localhost/heute_remote_site_id_1/check_mk/",
+                "http://localhost/heute_remote_site_id_2/check_mk/",
+                "http://localhost/heute_remote_site_id_3/check_mk/",
+                "http://localhost/heute_remote_site_id_4/check_mk/",
+                "http://localhost/heute_remote_site_id_5/check_mk/",
+                "http://localhost/heute_remote_site_id_6/check_mk/",
+                "http://localhost/heute_remote_site_id_7/check_mk/",
+                "http://localhost/heute_remote_site_id_8/check_mk/",
+                "http://localhost/heute_remote_site_id_9/check_mk/",
+                "http://localhost/heute_remote_site_id_10/check_mk/",
+                "https://localhost/hay/check_mk/"
+
+        ] , 
+
+        "ip_domain" : [
+                 "123.124.1.1",
+                 "123.124.1.2",
+                 "123.124.1.3",
+                 "123.124.1.4",
+                 "123.124.1.5",
+                  "123.124.1.6",
+                   "123.124.1.7",
+                    "123.124.1.8",
+                     "123.124.1.9",
+                      "123.124.1.10",
+                      "127.0.0.1"
+
+        ]      
+                
+                }
+
+
